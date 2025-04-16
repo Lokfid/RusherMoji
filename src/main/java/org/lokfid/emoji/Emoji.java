81e4b5c12c8ad1a2f0720f6123f634092a20ac85
Lokfid <n.pushkareva_82@mail.ru>
2025-04-16 15:33:18 +0500

    make emoji disappear with chat

--- src/main/java/org/lokfid/emoji/Emoji.java
+++ src/main/java/org/lokfid/emoji/Emoji.java
@@ -1,8 +1,10 @@
 package org.lokfid.emoji;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
+import org.lokfid.EmojiPlugin;
 
 public class Emoji {
 
@@ -13,6 +15,16 @@ public class Emoji {
     }
 
     public void render(GuiGraphics context, Font font, int x, int y, int color){
+
+        //So context.blit doesn't have a way to make the texture transparent. So we do this
+        float alpha = ((color >> 24) & 0xFF) / 255.0F;
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, alpha);
+
+        //Render the texture itself
         context.blit(resourceLocation, x, y - font.lineHeight / 8, 0, 0, font.lineHeight, font.lineHeight, font.lineHeight, font.lineHeight);
+
+        RenderSystem.disableBlend();
     }
 }
